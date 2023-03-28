1e628da204205b05f5abe3be67e702a3fd4c56df
antunishdPursuit <dennysantunish@pursuit.org>
2023-03-28 16:48:16 -0400

    The previous searches links are working

--- main.js
+++ main.js
@@ -91,30 +91,85 @@ const test = (json,city, cityArr) => {
             let min = json.weather[index].mintempF
             mines[index].innerHTML = `<strong class ="underline">Min Temperature:</strong> ${min} °F`
             }
-            
+            // Needed to call Api again so that the onclick can work
             let previousCitySearches = document.querySelectorAll("#previousCity")
             for (let index = 0; index < cityArr.length; index++) {
                 const element = previousCitySearches[index];
-                console.log(element)
-                console.log(cityArr)
-                element.addEventListener("click",(event)=>{
-                    let cityPreviousName = event.target.textContent
-                        console.log(cityPreviousName)
-                        test2(json,cityPreviousName)
+                element.onclick = function(event){
+                    let city2 =event.target.textContent
+                    let weatherData2 = `https://wttr.in/${city2}?format=j1`
+                    fetch(weatherData2)
+                    .then((response) => response.json())
+                    .then((json2) => {
+                        // the function which uses the API data
+                    test2(json2,city2)
+                        
                     })
+                    .catch((error) => {
+                    //   console.log(error);
+                
+                    });
+                    return false 
+                }
             }
         
 }
 
 const previousSearches = (city, currently) => {
+
         // selecting the aside tag which has all the li tags within in it 
-    document.querySelector("#removed").className = "hidden"
-    document.querySelector("#temps").className = "tempBoxes"
-    let searchList = document.querySelector("#list")
-    let previousSearches = document.createElement("li")
-    previousSearches.innerHTML = `<a href="#" id = "previousCity">${city}</a> - ${currently}`
-    previousSearches.className = "searchList"
-    // console.log(previousSearches.textContent)
-    searchList.append(previousSearches)
+        excuted = true
+        document.querySelector("#removed").className = "hidden"
+        document.querySelector("#temps").className = "tempBoxes"
+        let searchList = document.querySelector("#list")
+        let previousSearches = document.createElement("li")
+        previousSearches.innerHTML = `<a id = "previousCity" href="#">${city}</a> - ${currently}`
+        previousSearches.className = "searchList"
+        // console.log(previousSearches.textContent)
+        searchList.append(previousSearches)
+    
 }
 
+const test2 = (json,city) => {
+    // the first h1 tag becomes the input value
+    cityName.textContent = city
+    //Creating the first box of Weather Info
+    // creating varaibls for ease of use 
+    let areaDate = json.nearest_area[0]
+    let currentTempData = json.current_condition[0]
+    let area = areaDate.areaName[0].value
+    let testing = document.querySelector(".areaOfCountry")
+    // console.log(testing)
+    areap.innerHTML = `<strong>Area:</strong> ${area}`
+    let region = areaDate.region[0].value
+    regionp.innerHTML = `<strong>Region:</strong> ${region}`
+    cityInfo.append(regionp)
+    let country = areaDate.country[0].value
+    countryp.innerHTML = `<strong>Country:</strong> ${country}`
+    cityInfo.append(countryp)
+    let currently = currentTempData.FeelsLikeF + '°F'
+    currentlyp.innerHTML = `<strong>Currently:</strong> Feels like ${currently}`
+    cityInfo.append(currentlyp)
+    // adding pevious serches to a list
+    // previousSearches(city,currently)
+
+    // Adding temps for the today, next day, and next day
+    let threeDays = ["Today","Tomorrow","Day After Tomorrow"]
+
+    for (let index = 0; index < threeDays.length; index++) {
+        // Creating the date for the info
+        dates.setDate(test1.getDate() + index)
+        let time = dates.toLocaleDateString("en-US")
+        // created the html for each li and inputed the weather data
+        days[index].innerHTML = `<strong class ="underline">${threeDays[index]}:</strong> ${time}`
+        let avg = json.weather[index].avgtempF
+        avgs[index].innerHTML = `<strong class ="underline">Average Temperature:</strong> ${avg} °F`
+        let max = json.weather[index].maxtempF
+        maxes[index].innerHTML = `<strong class ="underline">Max Temperature:</strong> ${max} °F`
+        let min = json.weather[index].mintempF
+        mines[index].innerHTML = `<strong class ="underline">Min Temperature:</strong> ${min} °F`
+        }
+        //
+
+    
+}
