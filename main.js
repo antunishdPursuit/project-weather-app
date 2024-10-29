0609a983afca7bc35aaea281859ff1bf11c2e45d
antunishdPursuit <dennysantunish@pursuit.org>
2024-10-29 16:39:00 -0400

    add weather conditions and time

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let days = document.querySelectorAll(".days")
 let avgs = document.querySelectorAll(".avg")
 let maxes = document.querySelectorAll(".max")
 let mines = document.querySelectorAll(".min")
+let conditions = document.querySelectorAll(".condition")
 
 // selecting the aside tag with the id "temps"
 let temps = document.querySelector("#temps");
@@ -23,6 +24,9 @@ let cityInfo = document.querySelector("#cityInfo")
 
 // Used to reset all values when the input value is an empty string
 const lists = document.querySelectorAll(".lists");
+
+// Select all cloud elements
+let clouds = document.querySelectorAll(".cloud");
 
 // creating an array to store the cities that have been searched
 let cityArr = []
@@ -37,6 +41,12 @@ let cityWeatherObjects = {}
 let countOfSubmit = 0
 // Creating the Date for the weather 
 let dates = new Date();
+let timeString = dates.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+});
+
 function formatDate(date){
     return date.toLocaleDateString('en-US')
 }
@@ -64,7 +74,7 @@ weatherCity.addEventListener('submit', (event) => {
         fetch(weatherData)
         .then((response) =>{
             if (!response.ok) {
-                cityInfo.innerHTML = "<h2 id='cityName'>City Invalidr</h2>";
+                cityInfo.innerHTML = "<h2 id='cityName'>City Invalid</h2>";
                 lists.forEach(li => li.textContent = ""); 
                 throw new Error(`Error: ${response.status} ${response.statusText}`);
             }
@@ -101,9 +111,10 @@ const cityWeather = (json, city) => {
     //Creating the box of Weather Info  
     cityName.innerHTML = `<strong>${city}</strong>`
     areap.innerHTML = `<strong>Area:</strong> ${area}`
-    regionp.innerHTML = `<strong>Region:</strong> ${region}`
+    {region ? regionp.innerHTML = `<strong>Region:</strong> ${region}` : regionp.innerHTML = ""}
     countryp.innerHTML = `${country}`
-    currentlyp.innerHTML = `Feels like ${currentTemp} is  ${currentWeather}`
+    currentlyp.innerHTML = `<strong style="margin: 0;">${currentWeather}&nbsp;</strong> Feels like <strong>&nbsp;${currentTemp}</strong> at <strong>&nbsp;${timeString}</strong>`;
+
     degreeButton.classList.add("degreeCToF")
     degreeButton.innerHTML = `${fahrenheit ? "°C?" : "°F?"}`
 
@@ -117,15 +128,21 @@ const cityWeather = (json, city) => {
         let avg = json.weather[day].avgtempF
         let max = json.weather[day].maxtempF
         let min = json.weather[day].mintempF
+        let condition = json.weather[day].hourly[4].weatherDesc[0].value
         if (countOfSubmit <= 1){
             dates.setDate(dates.getDate() + day);
             let formattedDate = formatDate(dates)
             // 3 days worth of  data
-            days[day].innerHTML = `<strong>${threeDays[day]}:</strong> ${formattedDate}`
+            days[day].innerHTML = `<strong>${formattedDate}</strong> `
         }
         avgs[day].innerHTML = `<strong>Avg Temp:</strong> ${avg} °F`
         maxes[day].innerHTML = `<strong>Max Temp:</strong> ${max} °F`
         mines[day].innerHTML = `<strong>Min Temp:</strong> ${min} °F`
+        if(day === 0 ){
+            conditions[day].innerHTML = ``
+        } else {
+            conditions[day].innerHTML = `At 12:00 PM <strong>${condition}</strong>`
+        }
     }
 }
 
@@ -134,33 +151,30 @@ function changeBackground(weatherCode) {
     .then(response => response.json())
     .then(data => {
         const weatherIcons = data;
-        // Select all cloud elements
-        let clouds = document.querySelectorAll(".cloud");
         // Update background image for each cloud
         clouds.forEach(cloud => {
-            cloud.style.backgroundImage = `${weatherIcons[weatherCode]}`; // Path to new image
+            cloud.style.backgroundImage = `${weatherIcons[weatherCode]}`; 
         });
     })
     .catch(error => console.error("Error loading JSON:", error));
-
 }
+
 // adds all the cities searched onto the web page
 const previousSearches = (city, currently) => {
     // selecting the aside tag which has all the li tags within in it 
     document.querySelector("#removed").classList.add("hidden")
 
     if(!cityArr.includes(city)){
+        let searchList = document.querySelector("#list")
+        let previousSearch = document.createElement("li")
+        previousSearch.innerHTML = `
+        <a class="previousCity" href="#">${city}</a> 
+        <span class="current-temp">🌡️ ${currently}</span>
+        `;
+        previousSearch.className = "searchList"
+        searchList.append(previousSearch)
         cityArr.push(city)
     }
-
-    let searchList = document.querySelector("#list")
-    let previousSearch = document.createElement("li")
-    previousSearch.innerHTML = `
-    <a class="previousCity" href="#">${city}</a> - 
-    <span class="current-temp">🌡️ ${currently}</span>
-    `;
-    previousSearch.className = "searchList"
-    searchList.append(previousSearch)
 }
 
 // Needed to call Api again so that the onclick can work
@@ -171,7 +185,9 @@ if(!!previousCitySearches){
         if (event.target.classList.contains("previousCity")) {
             let previousCity = event.target.innerHTML
             cityWeatherObj(cityWeatherObjects[previousCity], previousCity)
+            funHea(previousCity)
         }
+
     });
 }
 
@@ -224,9 +240,9 @@ const cityWeatherObj = (json, city) => {
     //Creating the box of Weather Info  
     cityName.innerHTML = `<strong>${city}</strong>`
     areap.innerHTML = `<strong>Area:</strong> ${area}`
-    regionp.innerHTML = `<strong>Region:</strong> ${region}`
+    {region ? regionp.innerHTML = `<strong>Region:</strong> ${region}` : regionp.innerHTML = ""}
     countryp.innerHTML = `${country}`
-    currentlyp.innerHTML = `Feels like ${currentTemp} is ${currentWeather}`
+    currentlyp.innerHTML = `<strong style="margin: 0;">${currentWeather}&nbsp;</strong> Feels like <strong>&nbsp;${currentTemp}</strong> at <strong>&nbsp;${timeString}</strong>`;
     degreeButton.classList.add("degreeCToF")
     degreeButton.innerHTML = `${fahrenheit ? "°C?" : "°F?"}`
     // Adding the created HTML to the DOM
@@ -239,6 +255,7 @@ const cityWeatherObj = (json, city) => {
         let avg = json.weather[day].avgtempF
         let max = json.weather[day].maxtempF
         let min = json.weather[day].mintempF
+        let condition = json.weather[day].hourly[4].weatherDesc[0].value
         if(fahrenheit){
             avg = json.weather[day].avgtempF + " °F"
             max = json.weather[day].maxtempF + " °F"
@@ -252,6 +269,27 @@ const cityWeatherObj = (json, city) => {
         avgs[day].innerHTML = `<strong>Avg Temp:</strong> ${avg}`
         maxes[day].innerHTML = `<strong>Max Temp:</strong> ${max}`
         mines[day].innerHTML = `<strong>Min Temp:</strong> ${min}`
+        if(day === 0 ){
+            conditions[day].innerHTML = ``
+        } else {
+            conditions[day].innerHTML = `At 12:00 PM <strong>${condition}</strong>`
+        }
     }
 
+}
+
+function funHea(capitalizeCity){
+    const surpiseSVG = `
+    <svg width="30" height="30" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg" 
+        style="position: absolute; top: 10px; left: 10px;">
+        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+    </svg>
+`;
+
+if(capitalizeCity === "Ho Chi Minh City"){
+        clouds.forEach(cloud => {
+            cloud.innerHTML += surpiseSVG;
+        });
+        cityName.innerHTML = `Love U</strong>`
+    }
 }
